06c91e0315d8d3fb46c93e02311f2fae8a6b6371
Pocket <119549809+toshtag@users.noreply.github.com>
2026-07-29 16:14:20 +0900

    test(e2e): Playwright による打刻の画面テストを追加
    E2E は専用のデータベースとポートを使い、開発用や統合テスト用と混ざらないようにする。

--- docker/db-init/01-create-test-database.sh
+++ docker/db-init/01-create-test-database.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-# 統合テスト用のデータベースを初回起動時に作成する。
-# 開発用データベースとテスト用データベースを分けることで、テストが開発データを壊さないようにする。
+# 検証用のデータベースを初回起動時に作成する。
+# 開発用と分けることで、テストが開発データを壊さないようにする。
 set -eu
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 	CREATE DATABASE staffweave_test OWNER $POSTGRES_USER;
+	CREATE DATABASE staffweave_e2e OWNER $POSTGRES_USER;
 EOSQL
