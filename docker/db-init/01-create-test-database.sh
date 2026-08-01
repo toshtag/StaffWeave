0fe77129c12601fcd8cddddad5fd903c3e39adb6
Pocket <119549809+toshtag@users.noreply.github.com>
2026-08-01 23:18:31 +0900

    fix: データベースの資格情報と公開先を .env から決める

--- docker/db-init/01-create-test-database.sh
+++ docker/db-init/01-create-test-database.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 # 検証用のデータベースを初回起動時に作成する。
 # 開発用と分けることで、テストが開発データを壊さないようにする。
+#
+# 名前は開発用のデータベース名から作る。POSTGRES_DB を変えたときに、
+# 検証用だけ既定の名前で残らないようにする。
 set -eu
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE DATABASE staffweave_test OWNER $POSTGRES_USER;
-	CREATE DATABASE staffweave_e2e OWNER $POSTGRES_USER;
+	CREATE DATABASE "${POSTGRES_DB}_test" OWNER $POSTGRES_USER;
+	CREATE DATABASE "${POSTGRES_DB}_e2e" OWNER $POSTGRES_USER;
 EOSQL
